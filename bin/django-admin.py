a5932f3a83c17e4121d2869d14fdede8fd82bad6
Richard Guan <rguan3@gmail.com>
2014-06-01 01:23:03 -0700

    asdf

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Pilot/Desktop/django/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.6.4','django-admin.py'
+__requires__ = 'Django==1.6.4'
+import pkg_resources
+pkg_resources.run_script('Django==1.6.4', 'django-admin.py')
